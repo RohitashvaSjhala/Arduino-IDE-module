b7053980d5ba04e67e518aac6bb19b184def39be
RohitashvaSjhala <50163848+RohitashvaSjhala@users.noreply.github.com>
2019-05-01 14:44:24 +0530

    Update Track_Alter.ino

--- Track_Alter.ino
+++ Track_Alter.ino
@@ -1,4 +1,4 @@
-#include <AltSoftSerial.h> //Include all relevant libraries - see above
+#include <AltSoftSerial.h> 
 #include <TinyGPS++.h> 
 #include <GSM.h>
 #define PINNUMBER "1234" // PIN Number for the SIM - leave blank unless your SIM has a pin, this is inserted between ""
@@ -10,10 +10,10 @@ TinyGPSPlus gps; // The TinyGPS++ object for interfacing with the GPS
 
 AltSoftSerial ss; // The serial connection object to the GPS device
 
-String yourPassword = "alpacho"; // Put the password here between the ""
-String password; // Temporary variable used for comparison of passwords
+String yourPassword = "alpacho";
+String password;
 
-GSM gsmAccess; // Initialise the library instances
+GSM gsmAccess; 
 GSM_SMS sms;
 
 char senderNumber[20]; // Array to hold the number a SMS is retreived from
@@ -22,14 +22,14 @@ void setup()
 {
   ss.begin(GPSBaud); // begin the GPS serial connection
   
-  Serial.begin(9600); // begin Serial communication with the computer at 9600 baud rate
+  Serial.begin(9600); 
 
-  Serial.println("Where's my wheels?"); // Print to the computer
+  Serial.println("Where's my wheels?"); 
   Serial.println("Initialising..."); 
   
   boolean notConnected = true; // connection state
 
-  while(notConnected) // until it connects
+  while(notConnected) 
   {
     
     if(gsmAccess.begin(PINNUMBER)==GSM_READY)// if it succeeds connecting
@@ -37,12 +37,12 @@ void setup()
      
     else
     {
-      Serial.println("Not connected"); // print to the computer
-      delay(1000); //delay
+      Serial.println("Not connected"); 
+      delay(1000); 
     }
   }
 
-  Serial.println("GSM initialized");      // print to the computer
+  Serial.println("GSM initialized");   
   Serial.println("Waiting for messages");
 }
   
@@ -56,12 +56,12 @@ void loop()
  
   if (sms.available()) // if a text has been recieved
   {
-    Serial.println("Message received from:"); // print to the computer
+    Serial.println("Message received from:"); 
 
     sms.remoteNumber(+917740939329, 20); // assign the sender number to the "senderNumber" variable 
-    Serial.println(senderNumber); // print the sender number to the computer
+    Serial.println(senderNumber);
     
-    password = ""; // flush the temporary variable
+    password = ""; 
 
     char c;
     
@@ -70,15 +70,15 @@ void loop()
       password += c; // append the sms to the "password" variable
     }
 
-    Serial.println(password); // print the contents of the sms
-    Serial.println("\nEND OF MESSAGE"); // print to the computer
+    Serial.println(password); 
+    Serial.println("\nEND OF MESSAGE"); 
 
     sms.flush(); // delete message from modem buffer
-    Serial.println("MESSAGE DELETED"); // print to the computer
+    Serial.println("MESSAGE DELETED");
 
     //if (password == yourPassword) // if the sms contains the correct password
     {
-      Serial.println("\nPASSWORD VALID"); // print to the computer
+      Serial.println("\nPASSWORD VALID");
       sms.beginSMS(senderNumber); // begin an sms to the sender number
       sms.print(gps.location.lat(), 6); // append the lat to the sms
       sms.print(","); // append a comma
@@ -88,6 +88,6 @@ void loop()
 //    else Serial.println("\nPASSWORD NOT VALID"); // print to the computer
   }
 
-  delay(1000); // delay
+  delay(1000);
 }
 //-----+
